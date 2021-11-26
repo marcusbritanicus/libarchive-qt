6c01b4a937364e900a538aeef6b5599da12c97d3
Marcus Britanicus <marcusbritanicus@gmail.com>
2021-11-26 23:44:26 +0530

    Fix code style

--- lib/LibLzma2.cpp
+++ lib/LibLzma2.cpp
@@ -1,27 +1,27 @@
 /*
-	*
-	* Copyright 2018 Britanicus <marcusbritanicus@gmail.com>
-	*
-
-	*
-	* This program is free software: you can redistribute it and/or modify
-	* it under the terms of the GNU Lesser General Public License as published by
-	* the Free Software Foundation, either version 3 of the License, or
-	* (at your option) any later version.
-	*
-
-	*
-	* This program is distributed in the hope that it will be useful,
-	* but WITHOUT ANY WARRANTY; without even the implied warranty of
-	* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	* GNU Lesser General Public License for more details.
-	*
-
-	*
-	* You should have received a copy of the GNU Lesser General Public License
-	* along with this program.  If not, see <http://www.gnu.org/licenses/>.
-	*
-*/
+  *
+  * Copyright 2018 Britanicus <marcusbritanicus@gmail.com>
+  *
+  *
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU Lesser General Public License as published by
+  * the Free Software Foundation, either version 3 of the License, or
+  * (at your option) any later version.
+  *
+  *
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU Lesser General Public License for more details.
+  *
+  *
+  *
+  * You should have received a copy of the GNU Lesser General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  *
+  */
 
 // Local Headers
 #include <LibLzma2.hpp>
@@ -29,91 +29,110 @@
 const int MAX_READ_SIZE = 40960;
 
 QString NBXz::xzFileName = QString();
-QString NBXz::fileName = QString();
+QString NBXz::fileName   = QString();
 
-NBXz::NBXz( QString archive, QString file ) {
+NBXz::NBXz(QString archive, QString file)
+{
+	xzFileName = QString(archive);
 
-	xzFileName = QString( archive );
-
-	if ( not file.isEmpty() ) {
-		if ( QFileInfo( file ).isDir() ) {
-			fileName = QDir( file ).filePath( QString( archive ) );
-			fileName.chop( 3 );
+	if (not file.isEmpty())
+	{
+		if (QFileInfo(file).isDir())
+		{
+			fileName = QDir(file).filePath(QString(archive));
+			fileName.chop(3);
 		}
 
-		else if ( QFileInfo( file ).exists() ) {
-			QFile::rename( file, file + ".old" );
-			fileName = QString( file );
+		else if (QFileInfo(file).exists())
+		{
+			QFile::rename(file, file + ".old");
+			fileName = QString(file);
 		}
 
-		else {
-			fileName = QString( file );
+		else
+		{
+			fileName = QString(file);
 		}
 	}
 
-	else {
+	else
+	{
 		fileName = file;
-		fileName.chop( 3 );
+		fileName.chop(3);
 	}
 
-	fdin = fopen( qPrintable( xzFileName ), "rb" );
-	fdout = fopen( qPrintable( fileName ), "wb" );
-};
+	fdin  = fopen(qPrintable(xzFileName), "rb");
+	fdout = fopen(qPrintable(fileName), "wb");
+}
 
-bool NBXz::extract() {
 
+bool NBXz::extract()
+{
 	lzma_stream strm = LZMA_STREAM_INIT;
-	lzma_ret ret;
+	lzma_ret    ret;
 
 	// Initialize the decoder
-	ret = lzma_stream_decoder( &strm, UINT64_MAX, LZMA_CONCATENATED );
+	ret = lzma_stream_decoder(&strm, UINT64_MAX, LZMA_CONCATENATED);
 
-	if ( ret != LZMA_OK )
+	if (ret != LZMA_OK)
+	{
 		return false;
+	}
 
-	uint8_t in_buf[ MAX_READ_SIZE ];
-	uint8_t out_buf[ MAX_READ_SIZE ];
+	uint8_t in_buf[MAX_READ_SIZE];
+	uint8_t out_buf[MAX_READ_SIZE];
 
-	strm.avail_in = 0;
-	strm.next_out = out_buf;
+	strm.avail_in  = 0;
+	strm.next_out  = out_buf;
 	strm.avail_out = MAX_READ_SIZE;
 
 	lzma_action action = LZMA_RUN;
 
-	while ( true ) {
-		if ( strm.avail_in == 0 ) {
-			strm.next_in = in_buf;
-			strm.avail_in = fread( in_buf, 1, MAX_READ_SIZE, fdin );
+	while (true)
+	{
+		if (strm.avail_in == 0)
+		{
+			strm.next_in  = in_buf;
+			strm.avail_in = fread(in_buf, 1, MAX_READ_SIZE, fdin);
 
-			if ( ferror( fdin ) )
+			if (ferror(fdin))
+			{
 				return false;
+			}
 		}
 
-		if ( feof( fdin ) )
+		if (feof(fdin))
+		{
 			action = LZMA_FINISH;
+		}
 
-		ret = lzma_code( &strm, action );
+		ret = lzma_code(&strm, action);
 
-		if ( strm.avail_out == 0 || ret != LZMA_OK ) {
+		if ((strm.avail_out == 0) || (ret != LZMA_OK))
+		{
 			const size_t write_size = MAX_READ_SIZE - strm.avail_out;
 
-			if ( fwrite( out_buf, 1, write_size, fdout ) != write_size )
+			if (fwrite(out_buf, 1, write_size, fdout) != write_size)
+			{
 				return false;
+			}
 
-			strm.next_out = out_buf;
+			strm.next_out  = out_buf;
 			strm.avail_out = MAX_READ_SIZE;
 		}
 
-		if ( ret != LZMA_OK ) {
-			if ( ret == LZMA_STREAM_END ) {
+		if (ret != LZMA_OK)
+		{
+			if (ret == LZMA_STREAM_END)
+			{
 				// lzma_stream_decoder() already guarantees that there's no trailing garbage.
-				assert( strm.avail_in == 0 );
-				assert( action == LZMA_FINISH );
-				assert( feof( fdin ) );
+				assert(strm.avail_in == 0);
+				assert(action == LZMA_FINISH);
+				assert(feof(fdin));
 				return false;
 			}
 		}
 	}
 
 	return true;
-};
+}
--- lib/lz4dec.h
+++ lib/lz4dec.h
@@ -1,9 +1,9 @@
 /*
-	*
-	* lz4dec.h - Lz4 decompression header
-	*
-*/
+  *
+  * lz4dec.h - Lz4 decompression header
+  *
+  */
 
 #pragma once
 
-void unlz4( const char*, const char*, const char* );
+void unlz4(const char *, const char *, const char *);
